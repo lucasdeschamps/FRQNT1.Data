0c296c24e8c275190bf9c85c4dfcc59523e317a0
Lucas Deschamps <lucas.deschamps2@gmail.com>
2021-03-17 10:26:35 -0400

    Added data for density and biomasses

--- data-raw/Vascular_Abundances.R
+++ data-raw/Vascular_Abundances.R
@@ -2,3 +2,23 @@
 
 usethis::use_data(Vascular_Abundances, overwrite = TRUE)
 
+## Source cleaning function
+source("R/data.cleaning.R")
+
+
+# Import dataset containing vascular plant traits -------------------------
+Traits <- readr::read_csv2("data-raw/Vascular_Traits.csv")
+
+## Clean Trait characters
+Traits_clean <- data.cleaning(Traits) %>%
+  ## Select needed columns
+  select(Parcelle, Traitement, Exclos, Herbivorie, Sp,
+         BIOSHOOT_sec, Leaf_Msec)
+
+
+# Import data set containing vascular plants density ----------------------
+
+Dens <- readr::read_csv2("data-raw/Vascular_Density.csv")
+
+## Clean density characters
+Dens_clean <- data.cleaning(Dens)
