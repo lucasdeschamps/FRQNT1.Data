4659db1470e55a77f1f89c6509203583f73c9071
Lucas Deschamps <lucas.deschamps2@gmail.com>
2021-03-19 11:48:01 -0400

    Test to see if I can load data after github installation

---
+++ R/data.R
@@ -0,0 +1,15 @@
+#' @title DATASET_TITLE
+#' @description DATASET_DESCRIPTION
+#' @format A data frame with 1327 rows and 8 variables:
+#' \describe{
+#'   \item{\code{Parcelle}}{character COLUMN_DESCRIPTION}
+#'   \item{\code{Traitement}}{character COLUMN_DESCRIPTION}
+#'   \item{\code{Exclos}}{character COLUMN_DESCRIPTION}
+#'   \item{\code{Herbivorie}}{character COLUMN_DESCRIPTION}
+#'   \item{\code{Sp}}{character COLUMN_DESCRIPTION}
+#'   \item{\code{Nbr_Tiges}}{double COLUMN_DESCRIPTION}
+#'   \item{\code{BIOSHOOT_sec}}{double COLUMN_DESCRIPTION}
+#'   \item{\code{Leaf_Msec}}{double COLUMN_DESCRIPTION}
+#'}
+#' @source \url{http://somewhere.important.com/}
+"Vascular_Abundances_Clod"
--- data-raw/Vascular_Abundances.R
+++ data-raw/Vascular_Abundances.R
@@ -1,13 +1,13 @@
 ## code to prepare `Vascular_Abundances` dataset goes here
 
-usethis::use_data(Vascular_Abundances, overwrite = TRUE)
-
 ## Source cleaning function
 source("R/data.cleaning.R")
 
+library(tidyverse)
+
 
 # Import dataset containing vascular plant traits -------------------------
-Traits <- readr::read_csv2("data-raw/Vascular_Traits.csv")
+Traits <- readr::read_csv2("data-raw/Traits/Vascular_Traits.csv")
 
 ## Clean Trait characters
 Traits_clean <- data.cleaning(Traits) %>%
@@ -17,11 +17,14 @@ Traits_clean <- data.cleaning(Traits) %>%
 
 
 # Import data set containing vascular plants density ----------------------
-
-Dens <- readr::read_csv2("data-raw/Vascular_Density.csv")
+Dens <- readr::read_csv2("data-raw/Abundances/Vascular_Density.csv")
 
 ## Clean density characters
 Dens_clean <- data.cleaning(Dens) %>%
   select(Parcelle, Traitement, Exclos, Herbivorie, Sp, Nbr_Tiges)
+
+Vascular_Abundances_Clod <- left_join(Dens_clean, Traits_clean)
+
+usethis::use_data(Vascular_Abundances_Clod, overwrite = TRUE)
 
 
