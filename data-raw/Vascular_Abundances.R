c7baaa64daaae832cec805ee73d39cb12e5416d0
Lucas Deschamps <lucas.deschamps2@gmail.com>
2021-03-24 10:09:32 -0400

    Added the LAI computation, but there is some missing Nbr_Tiges. Have to check in samples

--- data-raw/Vascular_Abundances.R
+++ data-raw/Vascular_Abundances.R
@@ -1,6 +1,9 @@
 ## code to prepare `Vascular_Abundances` dataset goes here
 
-## Source cleaning function
+# Empty the environment
+rm(list = ls())
+
+# Source cleaning function
 source("R/data.cleaning.R")
 
 library(tidyverse)
@@ -13,17 +16,77 @@ Traits <- readr::read_csv2("data-raw/Traits/Vascular_Traits.csv")
 Traits_clean <- data.cleaning(Traits) %>%
   ## Select needed columns
   select(Parcelle, Traitement, Exclos, Herbivorie, Sp,
-         BIOSHOOT_sec, Leaf_Msec)
+         BIOSHOOT_sec, Leaf_Msec, Poids_non_ligneux, Poids_1an,
+         Nbre_ind_feuille, Nbre_feuille_mature, Nbre_feuille_tot, `Subsampling?`,
+         Total_leaf_area, Leaf_number)
 
 
 # Import data set containing vascular plants density ----------------------
-Dens <- readr::read_csv2("data-raw/Abundances/Vascular_Density.csv")
+Dens <- readr::read_csv("data-raw/Abundances/Vascular_Density.csv")
 
 ## Clean density characters
 Dens_clean <- data.cleaning(Dens) %>%
-  select(Parcelle, Traitement, Exclos, Herbivorie, Sp, Nbr_Tiges)
+  select(Parcelle, Traitement, Exclos, Herbivorie, Motte, Motte_enveloppe, Sp, Nbr_Tiges)
 
+
+# Create final data set ---------------------------------------------------
+## Join the two databases together
 Vascular_Abundances_Clod <- left_join(Dens_clean, Traits_clean)
+
+Vascular_Abundances_Clod <- Vascular_Abundances_Clod %>%
+  #####
+  ## Compute total biomass and primary productivity per species per plot
+  mutate(Biomass_plot = select(., BIOSHOOT_sec, Leaf_Msec) %>% apply(1, sum, na.rm = T),
+         Productivity_plot = ifelse(is.na(Poids_non_ligneux),
+                                    Biomass_plot,
+                                    select(., Poids_non_ligneux, Leaf_Msec, Poids_1an) %>% apply(1, sum, na.rm = T)
+                                    )) %>%
+  #####
+  ## Interpolate the mass and PP of each species in each clod
+  ### Compute the number of tiller in a plot
+  group_by(Parcelle, Traitement, Exclos, Sp) %>%
+  mutate(Tiller_nbr_plots = sum(Nbr_Tiges, na.rm = T)) %>%
+  ungroup() %>%
+  ### Compute the mean mass and productivity of an individual
+  mutate(Biomass_ind_mean = Biomass_plot / Tiller_nbr_plots,
+         Productivity_ind_mean = Productivity_plot / Tiller_nbr_plots) %>%
+  ### Compute the mass and productivity of each species in each clod
+  mutate(Biomass = Biomass_ind_mean * Nbr_Tiges,
+         Productivity = Productivity_ind_mean * Nbr_Tiges) %>%
+  #####
+  ## Compute the leaf area of each species in each clod
+  ### Compute leaf area
+  mutate(LA = Total_leaf_area/Leaf_number) %>%
+  ### Compute mean leaf area per species per treatment
+  group_by(Traitement, Exclos, Sp) %>%
+  mutate(LA_mean_treat = mean(LA, na.rm = T)) %>%
+  ungroup() %>%
+  ### Compute mean leaf area per species
+  group_by(Sp) %>%
+  mutate(LA_mean_sp = mean(LA, na.rm = T)) %>%
+  #####
+  ## Compute the mean number of leaves per individuals for each species of each plot
+  ### Correct the number of individuals measured
+  mutate(Nbre_ind_feuille = ifelse(is.na(`Subsampling?`), Tiller_nbr_plots, 15)) %>%
+  ### Compute the mean number of leaves per individuals for each species in each plot
+  mutate(Nbre_feuille_ind_plot = ifelse(is.na(`Subsampling?`),
+                                    (Nbre_feuille_mature + Leaf_number)/ Nbre_ind_feuille, Nbre_feuille_mature/Nbre_ind_feuille)) %>%
+  ### Compute the mean number of leaves per individuals for each species in each treatment
+  group_by(Traitement, Exclos, Sp) %>%
+  mutate(Nbre_feuille_ind_treat = mean(Nbre_feuille_ind_plot, na.rm = T)) %>%
+  ### Compute the mean number of leaves per individuals for each species
+  group_by(Sp) %>%
+  mutate(Nbre_feuille_ind_sp = mean(Nbre_feuille_ind_plot, na.rm = T)) %>%
+  ##### Complete the LA and Nbre_feuille columns
+  mutate(LA_comp = ifelse(is.na(LA_mean_treat), LA_mean_sp, LA_mean_treat),
+         Nbre_feuille_ind_treat_comp = ifelse(is.na(Nbre_feuille_ind_treat), Nbre_feuille_ind_sp, Nbre_feuille_ind_treat),
+         Nbre_feuille_comp = ifelse(is.na(Nbre_feuille_ind_plot) | Nbre_ind_feuille < 10,
+                                    Nbre_feuille_ind_treat_comp, Nbre_feuille_ind_plot)) %>%
+  #####
+  ## Compute the LAI per species per clod
+  mutate(LAI = LA_comp * Nbre_feuille_comp * Nbr_Tiges)
+
+
 
 usethis::use_data(Vascular_Abundances_Clod, overwrite = TRUE)
 
