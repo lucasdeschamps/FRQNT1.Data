6a57000966ae95c08509d11adf2fd3558e0ebff1
Lucas Deschamps <lucas.deschamps2@gmail.com>
2021-04-16 12:12:00 -0400

    Changed the add.treatment function to use only dplyr to recode factors

--- R/add.treatments.R
+++ R/add.treatments.R
@@ -12,20 +12,21 @@ add.treatments <- function(D){
   require(tidyverse)
 
   D_added <- D %>%
-    mutate(Geomorpho = car::recode(Traitement, recodes = "c('1','4','6','10','14') = 'PH'"),
-           Fertilization_N = car::recode(Traitement, recodes = "c('1', 'PA', 'PR', 'PO', 'PS') = 0;
-                                  '4' = 1;'6' = 5;'10' = 0;'14' = 5"),
-           Fertilization_P = car::recode(Traitement,recodes = "c('1', '4', '6', 'PA', 'PR', 'PO', 'PS') = 0;
-                                  c('14') = 1; c('10') = 3"),
-           Fertilization = car::recode(Traitement, recodes = "c('1', 'PA', 'PR', 'PO', 'PS') = 0"),
+    mutate(Geomorpho = recode(Traitement, "1" = "PH", "4" = "PH", "6" = "PH", "10" = "PH", "14" = "PH"),
+           Fertilization_N = recode(Traitement, "1" = "0", PA = "0", PAS = "0", PR = "0", PO = "0", PS = "0",
+                                    "4" = "1", "6" = "6", "10" = "0", "14" = "5"),
+           Fertilization_P = recode(Traitement, "1" = "0", "4" = "0", "6" = "0",
+                                    PA = "0", PAS = "0", PR = "0", PO = "0", PS = "0",
+                                    "14" = "1", "10" = "3"),
+           Fertilization = recode(Traitement, "1" = "0", PA = "0", PAS = "0", PR = "0", PO = "0", PS = "0"),
            Traitement = ifelse(Parcelle %in% c("26", "43"), "PAS", Traitement)) %>%
     mutate(Geomorpho = replace(Geomorpho, Parcelle == "ROC6", "PS")) %>%
     mutate_at(vars(Fertilization_N, Fertilization_P, Fertilization), as.character) %>%
     mutate(Parcelle = factor(Parcelle),
-           Geomorpho = factor(Geomorpho, levels = c("PH", "PS", "PA", "PO", "PR")),
+           Geomorpho = factor(Geomorpho, levels = c("PH", "PS", "PA", "PAS", "PO", "PR")),
            Exclos = factor(Exclos, levels = c("Temoin", "Exclos")),
-           Fertilization = factor(Fertilization, levels = c(0,4,10, 6,14)),
-           Fertilization_N = ordered(Fertilization_N, levels = c(0,1,5)),
+           Fertilization = factor(Fertilization, levels = c(0,4,10,6,14)),
+           Fertilization_N = factor(Fertilization_N, levels = c(0,1,5)),
            Fertilization_P = factor(Fertilization_P, levels = c(0,1,3)),
            Fertilization_P_bin = factor(ifelse(Fertilization_P == 0, 0, 1))
     )
