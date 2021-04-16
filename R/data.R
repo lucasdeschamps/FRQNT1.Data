8d4f3ab03e5cd07091831970c07b45bc2c8f6d46
Lucas Deschamps <lucas.deschamps2@gmail.com>
2021-04-16 12:12:50 -0400

    Added the clean data set for vascular traits

--- R/data.R
+++ R/data.R
@@ -26,6 +26,48 @@
 #'   Computed indirectly as mean number of leaves per individual of the species in the plot multpiplied by
 #'  the mean leaf area of the species in the treatment and the number of individual in the clod}
 #'   \item{\code{LAI_rel}}{double LAI of the species in the clod divided by the total LAI in the clod}
+#'   #'   \item{\code{Geomorpho}}{integer A formulation of the Traitement columns focusing on discrete
+#'   habitat differences}
+#'   \item{\code{Fertilization_N}}{integer Specifies the quantity of N added each year in g.m-2 of NH4NO3}
+#'   \item{\code{Fertilization_P}}{integer Specifies the quantity of P added each year in g.m-2 of H3PO4}
+#'   \item{\code{Fertilization}}{integer A formulation of the Traitement columns with a zero to each unfertilized plots}
+#'   \item{\code{Fertilization_P_bin}}{integer Binary variable indicating if the plot receive annual P fertilization}
 #'}
 #' @source \url{http://somewhere.important.com/}
 "Vascular_Abundances_Clod"
+
+#' @title Traits of Vascular Plants in each plot
+#' @description This dataset contains the mean traits of each species in each plot,
+#' measured either on 10-12 individuals or on every individuals pulled together, depending on their abundances.
+#' @format A data frame with 340 rows and 22 variables:
+#' \describe{
+#'   \item{\code{Parcelle}}{integer Name of the site}
+#'   \item{\code{Traitement}}{character Name of the treatment. PA : Polygons slightly drained, PAS : Polygons drained by a secondary ice wedge,
+#'   PS : polygons close to the shore, submitted to abundant eolian silt deposition, PO : flat to high-center mesic polygons,  PR : mesic meadow,
+#'   1 : low-centered, wet polygon; 4 : wet polygons fertilized yearly with 1 g.m-2 of NH4NO3; 6 : wet polygons fertilized yearly with 1 g.m-2 of NH4NO3,
+#'   10 : wet polygons fertilized yearly with 3 g.m-2 of H3PO4; 14 : wet polygons fertilized yearly with 1 g.m-2 of H3PO4 amd 5 g.m-2 of NH4NO3}
+#'   \item{\code{Exclos}}{integer Exclos : plots protected constantly by a fence; Temoin : unprotected plots}
+#'   \item{\code{Herbivorie}}{character Grazing (reformulation of Exclos). Ungrazed : plots protected constantly by a fence; Grazed : unprotected plots}
+#'   \item{\code{Sp}}{character Code of species}
+#'   \item{\code{Leaf_N15}}{double Leaf d15N, in per mill}
+#'   \item{\code{LNC}}{double Leaf Nitrogen Content, in percent}
+#'   \item{\code{Leaf_C13}}{double Leaf d13C, in per mill}
+#'   \item{\code{LCC}}{double Leaf Carbon Content, in percent}
+#'   \item{\code{Root_N15}}{double Root d15N, in per mill}
+#'   \item{\code{RNC}}{double Root Nitrogen Content, in percent}
+#'   \item{\code{Leaf_pH}}{double Leaf Hydrogen potential}
+#'   \item{\code{Hveg}}{double Vegetative height in cm, measured as the distance between the base of the plant
+#'   and the extremity of the extended leaves. Measured on height individuals per plot or less}
+#'   \item{\code{LDMC}}{double Leaf Dry Matter Content, in g_dry.g_fresh^-1}
+#'   \item{\code{RDMC}}{double Root Dry Matter Content, in g_dry.g_fresh^-1}
+#'   \item{\code{LA}}{double Leaf Area, in cm^2}
+#'   \item{\code{SLA}}{double Specific Leaf Area, in cm^2.g^-1}
+#'   \item{\code{Geomorpho}}{integer A formulation of the Traitement columns focusing on discrete
+#'   habitat differences}
+#'   \item{\code{Fertilization_N}}{integer Specifies the quantity of N added each year in g.m-2 of NH4NO3}
+#'   \item{\code{Fertilization_P}}{integer Specifies the quantity of P added each year in g.m-2 of H3PO4}
+#'   \item{\code{Fertilization}}{integer A formulation of the Traitement columns with a zero to each unfertilized plots}
+#'   \item{\code{Fertilization_P_bin}}{integer Binary variable indicating if the plot receive annual P fertilization}
+#'}
+#' @source \url{http://somewhere.important.com/}
+"Vascular_Traits_Plot"
---
+++ data-raw/Vascular_Traits_Plot.R
@@ -0,0 +1,47 @@
+## code to prepare `Vascular_Traits_Plot` dataset goes here
+
+# Empty the environment
+rm(list = ls())
+
+# Source cleaning function
+source("R/data.cleaning.R")
+source("R/add.treatments.R")
+
+library(tidyverse)
+
+
+# Import dataset containing vascular plant traits -------------------------
+Traits <- readr::read_csv2("data-raw/Traits/Vascular_Traits.csv")
+
+## Clean Trait characters
+Vascular_Traits_Clod <- data.cleaning(Traits) %>%
+  ## Select needed columns
+  select(Parcelle, Traitement, Exclos, Herbivorie, Sp,
+         Leaf_N15, LNC, Leaf_C13, LCC, Root_N15, RNC, Leaf_pH,
+         Hveg1:Hveg8,
+         Leaf_Msec, Leaf_Mfresh, Root_Msec, Root_Mfresh,
+         Total_leaf_area, Leaf_number) %>%
+  ## Compute new traits
+  mutate(Hveg = rowMeans(
+    data.frame(Hveg1, Hveg2, Hveg3, Hveg4, Hveg5, Hveg6, Hveg7, Hveg8),
+    na.rm = T),
+    LDMC = Leaf_Msec/Leaf_Mfresh,
+    RDMC = Root_Msec/Root_Mfresh,
+    LA = Total_leaf_area/Leaf_number,
+    SLA = Total_leaf_area/Leaf_Msec
+    ) %>%
+  select(Parcelle, Traitement, Exclos, Herbivorie, Sp,
+         Leaf_N15, LNC, Leaf_C13, LCC, Root_N15, RNC, Leaf_pH,
+         Hveg, LDMC, RDMC, LA, SLA)
+
+# Summarise result per plot, as only a few plots have repeated mea --------
+Vascular_Traits_Plot <- Vascular_Traits_Clod %>%
+  group_by(Parcelle, Traitement, Exclos, Herbivorie, Sp) %>%
+  summarise_all(.funs = mean, na.rm = T)
+
+# Add Treatments variables
+Vascular_Traits_Plot <- add.treatments(Vascular_Traits_Plot)
+
+usethis::use_data(Vascular_Traits_Plot, overwrite = TRUE)
+
+
