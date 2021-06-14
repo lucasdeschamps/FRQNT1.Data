873ab808c23fbfd1caea36adaa4bb39a2e63b453
Lucas Deschamps <lucas.deschamps2@gmail.com>
2021-06-14 12:08:15 -0400

    Removed an useless column in the sliced dataset

--- data-raw/Soil_Physic_mm.R
+++ data-raw/Soil_Physic_mm.R
@@ -17,8 +17,8 @@ load("data/Soil_Physic_Horizons.rda")
 Soil_Physic_mm <- Soil_Physic_Horizons %>%
   group_by(Date, Parcelle, Traitement, Exclos, Depth_up, Depth_down) %>%
   ## Collapse all date, and use the vector to create new rows
-  mutate(Prof = paste(seq(Depth_up, Depth_down, by = 0.5), collapse = ",")) %>%
-  separate_rows(Prof, sep = ",") %>%
-  mutate(Depth = as.numeric(Prof))
+  mutate(Depth = paste(seq(Depth_up, Depth_down, by = 0.5), collapse = ",")) %>%
+  separate_rows(Depth, sep = ",") %>%
+  mutate(Depth = as.numeric(Depth))
 
 usethis::use_data(Soil_Physic_mm, overwrite = TRUE)
