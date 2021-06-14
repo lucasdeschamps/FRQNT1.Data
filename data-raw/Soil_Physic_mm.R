5d8bbcfccae264824e6e988943b63d1a598af076
Lucas Deschamps <lucas.deschamps2@gmail.com>
2021-06-14 11:47:35 -0400

    Added the dataset sliced by 5mm

---
+++ data-raw/Soil_Physic_mm.R
@@ -0,0 +1,24 @@
+## code to prepare `Vascular_Abundances_Clod` dataset goes here
+
+# Empty the environment
+rm(list = ls())
+
+# Source cleaning function
+source("R/data.cleaning.R")
+source("R/add.treatments.R")
+
+library(tidyverse)
+
+
+# Import dataset containing vascular plant traits -------------------------
+load("data/Soil_Physic_Horizons.rda")
+
+# Create final data set ---------------------------------------------------
+Soil_Physic_mm <- Soil_Physic_Horizons %>%
+  group_by(Date, Parcelle, Traitement, Exclos, Depth_up, Depth_down) %>%
+  ## Collapse all date, and use the vector to create new rows
+  mutate(Prof = paste(seq(Depth_up, Depth_down, by = 0.5), collapse = ",")) %>%
+  separate_rows(Prof, sep = ",") %>%
+  mutate(Depth = as.numeric(Prof))
+
+usethis::use_data(Soil_Physic_mm, overwrite = TRUE)
