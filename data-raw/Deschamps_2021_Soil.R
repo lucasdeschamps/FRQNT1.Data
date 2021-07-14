581336689cec930f81f0b1d43b3bc865a9275ba6
Lucas Deschamps <lucas.deschamps2@gmail.com>
2021-07-14 14:23:32 -0400

    Created the first version of predicted soil properties, with differences in SVWC

--- data-raw/Deschamps_2021_Soil.R
+++ data-raw/Deschamps_2021_Soil.R
@@ -12,6 +12,7 @@ library(tidyverse)
 library(lubridate)
 library(brms)
 library(emmeans)
+library(tidybayes)
 
 # Import models -----------------------------------------------------------
 fit_LOI <- readRDS("/home/lucasd/OneDrive/Projects/Active_projects/Doctorat_Lucas/Analysis/R/Results/Chap2/ANOVA/ANOVA_LOI.RDS")
@@ -33,29 +34,21 @@ for(d in 1:3){
   em_LOI <- emmeans(fit_LOI, "Fertilization", by = "Exclos",
                     at = list(Depth = upper[d]:lower[d]),
                     type = "response") %>%
-    as_tibble() %>%
-    filter(Fertilization %in% c(0,14)) %>%
-    rename(mean_LOI = response,
-           lower_LOI = lower.HPD,
-           upper_LOI = upper.HPD)
+    gather_emmeans_draws(value = "logit_LOI") %>%
+    filter(Fertilization %in% c(0,14))
+
   ## Compute Porosity ems
   em_Porosity <- emmeans(fit_Porosity, "Fertilization", by = "Exclos",
                     at = list(Depth = upper[d]:lower[d]),
                     type = "response") %>%
-    as_tibble() %>%
-    filter(Fertilization %in% c(0,14)) %>%
-    rename(mean_Porosity = response,
-           lower_Porosity = lower.HPD,
-           upper_Porosity = upper.HPD)
+    gather_emmeans_draws(value = "logit_Porosity") %>%
+    filter(Fertilization %in% c(0,14))
   ## Compute Density ems
   em_Density <- emmeans(fit_Density, "Fertilization", by = "Exclos",
                          at = list(Depth = upper[d]:lower[d]),
                          type = "response") %>%
-    as_tibble() %>%
-    filter(Fertilization %in% c(0,14)) %>%
-    rename(mean_Density = response,
-           lower_Density = lower.HPD,
-           upper_Density = upper.HPD)
+    gather_emmeans_draws(value = "log_Density") %>%
+    filter(Fertilization %in% c(0,14))
 
 
   if(d == 1) em_Soil <- left_join(em_LOI, em_Porosity) %>%
@@ -77,44 +70,43 @@ for(d in 1:3){
 em_VWC <- emmeans(fit_VWC, specs = "Fertilization", by = "Exclos",
                   cov.keep = "DOY",
                   type = "response") %>%
-  as_tibble() %>%
-  filter(Fertilization %in% c(0,14)) %>%
-  rename(mean_VWC = response,
-         lower_VWC = lower.HPD,
-         upper_VWC = upper.HPD)
+  gather_emmeans_draws(value = "logit_VWC") %>%
+  filter(Fertilization %in% c(0,14))
 
 em_WTD <- emmeans(fit_WTD, specs = "Fertilization", by = "Exclos",
                   cov.keep = "DOY",
                   type = "response") %>%
-  as_tibble() %>%
-  filter(Fertilization %in% c(0,14)) %>%
-  rename(mean_WTD = response,
-         lower_WTD = lower.HPD,
-         upper_WTD = upper.HPD)
+  gather_emmeans_draws(value = "log_WTD") %>%
+  filter(Fertilization %in% c(0,14))
 
 # Join everything together ------------------------------------------------
 Deschamps_2021_Soil <- left_join(em_Soil, em_VWC) %>%
-  left_join(em_WTD)
+  left_join(em_WTD) %>%
+  mutate(LOI = brms::inv_logit_scaled(logit_LOI),
+         Porosity = brms::inv_logit_scaled(logit_Porosity),
+         Density = exp(log_Density),
+         VWC = brms::inv_logit_scaled(logit_VWC),
+         WTD = exp(log_WTD))
 
 # Create a new mean WVC taking into account WTD ---------------------------
 Deschamps_2021_Soil <- Deschamps_2021_Soil %>%
-  mutate(mean_VWC = ifelse(Fertilization != "14" |
+  mutate(VWC = ifelse(Fertilization != "14" |
                              Exclos != "Exclos" |
                              Horizon %nin% c("10-15", "15-300"),
-         mean_VWC, mean_Porosity)) %>%
-  mutate(mean_VWC = ifelse(Fertilization != "14" |
+         VWC, Porosity)) %>%
+  mutate(VWC = ifelse(Fertilization != "14" |
                              Exclos != "Temoin" |
                              Horizon %nin% c("5-10","10-15", "15-300"),
-                           mean_VWC, mean_Porosity)) %>%
-  mutate(mean_VWC = ifelse(Fertilization != "0" |
+                           VWC, Porosity)) %>%
+  mutate(VWC = ifelse(Fertilization != "0" |
                              Exclos %nin% c("Temoin", "Exclos") |
                              Horizon %nin% c("5-10","10-15", "15-300"),
-                           mean_VWC, mean_Porosity))
+                           VWC, Porosity))
 
 
 # Compute thermal conductivity --------------------------------------------
 Deschamps_2021_Soil <- Deschamps_2021_Soil %>%
-  ### Create variables used in equation
+  ### Create parameters used in equations
   mutate(rho_om = 1.3,
          rho_min = 2.65,
          K_om = 0.25,
@@ -126,49 +118,57 @@ Deschamps_2021_Soil <- Deschamps_2021_Soil %>%
          alpha = 0.24,
          beta = 18.3) %>%
   ## Compute particle density
-  mutate(mean_Particle_Density = 1 / (mean_LOI/rho_om + (1-mean_LOI)/rho_min)) %>%
+  mutate(Particle_Density = 1 / (LOI/rho_om + (1-LOI)/rho_min)) %>%
   ## Compute OM and mineral volumetric fraction
-  mutate(mean_V_om = mean_LOI * (mean_Density/rho_om),
-         mean_V_min = (1-mean_LOI) * (mean_Density/rho_min)) %>%
+  mutate(V_om = LOI * (Density/rho_om),
+         V_min = (1-LOI) * (Density/rho_min)) %>%
   ### Compute volumetric fraction of soil solids
-  mutate(mean_V_oms = mean_V_om / (1-mean_Porosity),
-         mean_V_mins = 1-mean_V_oms) %>%
+  mutate(V_oms = V_om / (1-Porosity),
+         V_mins = 1-V_oms) %>%
   ### Compute the proportion of pores saturated by water
-  mutate(mean_theta_sat = mean_VWC/(mean_Porosity)) %>%
+  mutate(theta_sat = VWC/(Porosity),
+         theta_sat = ifelse(theta_sat > 1, 1, theta_sat)) %>%
   ### Compute thermal conductivity of solids
-  mutate(K_solid = K_om ^ mean_V_oms * K_min ^ mean_V_mins) %>%
+  mutate(K_solid = K_om ^ V_oms * K_min ^ V_mins) %>%
   ### Compute thermal conductivity of dry soil
-  mutate(K_dry = ( (a * K_solid - K_air) * mean_Density +
-                     K_air * mean_Particle_Density)/
-           (mean_Particle_Density - (1-a)*mean_Density)) %>%
+  mutate(K_dry = ( (a * K_solid - K_air) * Density +
+                     K_air * Particle_Density)/
+           (Particle_Density - (1-a)*Density)) %>%
   ### Compute the saturated conductivities
-  mutate(K_sat = K_solid ^ (1-mean_Porosity) * K_water^(mean_Porosity),
-         K_sat_frozen = K_solid ^ (1-mean_Porosity) * K_ice ^ mean_Porosity) %>%
+  mutate(K_sat = K_solid ^ (1-Porosity) * K_water^(Porosity),
+         K_sat_frozen = K_solid ^ (1-Porosity) * K_ice ^ Porosity) %>%
   ### Compute the contribution of water to thermal conductivity
-  mutate(K_e = (mean_theta_sat ^ (0.5 * (1 + mean_V_oms)) ) *
-           ( (1/(1 + exp(- beta * mean_theta_sat)))^3 -
-               ((1-mean_theta_sat)/2)^3  )^(1-mean_V_oms),
-         K_e_frozen = mean_theta_sat ^ (1+mean_V_oms)
+  mutate(K_e = (theta_sat ^ (0.5 * (1 + V_oms)) ) *
+           ( (1/(1 + exp(- beta * theta_sat)))^3 -
+               ((1-theta_sat)/2)^3  )^(1-V_oms),
+         K_e_frozen = theta_sat ^ (1+V_oms)
   ) %>%
   ### Compute predicted thermal conductivity
   mutate(K_soil = (K_sat - K_dry) * K_e + K_dry,
          K_soil_frozen = (K_sat_frozen - K_dry) * K_e_frozen + K_dry)
 
-# Make dataset its final form ---------------------------------------------
-Deschamps_2021_Forcing_Day <- Deschamps_2021_Forcing %>%
-  ## Summarise by date
-  group_by(Fertilization, Grazing, Date) %>%
-  summarise_at(vars(Year, Month, Day, DOY,
-                    `WindSpeed m/s`, `Air Temp, degC`,
-                    `Relative Humidity`, `Specific humidity g/kg`,
-                    `Long Wave Downwell, ERA5 W m-2`,
-                    `Short Wave Downwell, CNR4 W m-2`, `Short Wave downwell, ERA5 W m-2`,
-                    Net_ShortWave, Net_LongWave,
-                    `Pressure kPa`,
-                    `Precip, Total  mm/h`, `Precip, Rain`, `Precip, Snow`, `Precip  season cumul`,
-                    `Snow depth m`,
-                    Temp_11m,
-                    Shade_mod, x , FCC_mod, Albedo_ground),
-               .funs = mean)
+# Compute volumetric heat capacity ----------------------------------------
+Deschamps_2021_Soil <- Deschamps_2021_Soil %>%
+  ## Create parameters used in equations
+  mutate(C_om = 2.51e6,
+         C_min = 2.01e6,
+         C_air = 1297,
+         C_water = 4.180e6,
+         C_ice = 1.88e6) %>%
+  ## Compute the volumic fraction of water
+  mutate(V_air = 1 - theta_sat) %>%
+  ## Compute Volumic heat capacity
+  mutate(VolHeatCap = C_om * V_om + C_air * V_air + C_water * VWC + C_min * V_min,
+         VolHeatCap_frozen = C_om * V_om + C_air + C_air + C_ice * VWC + C_min * V_min) %>%
+  ## Compute thermal diffusivity
+  mutate(ThermDiff = K_soil/VolHeatCap,
+         ThermDiff_frozen = K_soil_frozen/VolHeatCap_frozen)
 
-usethis::use_data(Deschamps_2021_Forcing_Day, overwrite = TRUE)
+# Make dataset its final form ---------------------------------------------
+Deschamps_2021_Soil_draws <- Deschamps_2021_Soil %>%
+  ## Select relevant variables
+  select(-.chain, - .iteration) %>%
+  mutate(Horizon = factor(Horizon,
+                          levels = c("0-5", "5-10", "10-15", "15-300")))
+
+usethis::use_data(Deschamps_2021_Soil_draws, overwrite = TRUE)
---
+++ data-raw/Deschamps_2021_Soil_mean.R
@@ -0,0 +1,40 @@
+## code to prepare soil properties for Deschamps 2021 model
+
+# Empty the environment
+rm(list = ls())
+
+# Source cleaning function
+source("R/data.cleaning.R")
+source("R/add.treatments.R")
+source("R/misc.R")
+
+library(tidyverse)
+library(lubridate)
+library(brms)
+library(emmeans)
+library(tidybayes)
+
+
+# Import data set containing draws of soil physical properties ------------
+load("data/Deschamps_2021_Soil_draws.rda")
+
+# Compute the median of each variable -------------------------------------
+Deschamps_2021_Soil_mean <- Deschamps_2021_Soil_draws %>%
+  group_by(Fertilization, Exclos, Horizon) %>%
+  summarise_at(vars(logit_LOI, logit_Porosity, log_Density,
+                    logit_VWC, theta_sat,
+                    K_soil, VolHeatCap, ThermDiff,
+                    K_soil_frozen, VolHeatCap_frozen, ThermDiff_frozen),
+               .funs = median) %>%
+  mutate(LOI = brms::inv_logit_scaled(logit_LOI),
+         Porosity = brms::inv_logit_scaled(logit_Porosity),
+         Density = exp(log_Density),
+         VWC = brms::inv_logit_scaled(logit_VWC))
+
+
+# Make dataset in its final form ---------------------------------------------
+Deschamps_2021_Soil_mean <- Deschamps_2021_Soil_mean %>%
+  ## Select relevant variables
+  select(-logit_LOI:-logit_VWC)
+
+usethis::use_data(Deschamps_2021_Soil_mean, overwrite = TRUE)
